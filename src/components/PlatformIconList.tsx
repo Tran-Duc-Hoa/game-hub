885da203c1f29d96538b8fb66d3c6b268ca829b1
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2023-04-24 13:59:27 +0700

    Diplaying platform icon

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,48 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+	FaAndroid,
+	FaApple,
+	FaLinux,
+	FaPlaystation,
+	FaWindows,
+	FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+	platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+	const iconMap: { [key: string]: IconType } = {
+		windows: FaWindows,
+		playstation: FaPlaystation,
+		xbox: FaXbox,
+		ios: FaApple,
+		linux: FaLinux,
+		android: FaAndroid,
+		nintendo: SiNintendo,
+		mac: FaApple,
+		web: BsGlobe,
+		iphone: MdPhoneIphone,
+	};
+
+	return (
+		<HStack marginY={1}>
+			{platforms.map((platform) => (
+				<Icon
+					as={iconMap[platform.slug]}
+					key={platform.slug}
+					color="gray.500"
+				/>
+			))}
+		</HStack>
+	);
+};
+
+export default PlatformIconList;
