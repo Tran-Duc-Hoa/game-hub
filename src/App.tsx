db9e43668218c560592849cbe5f1cce125d80d5f
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2023-05-22 13:02:05 +0700

    Add a dynamic heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react';
 import { useState } from 'react';
 
 import GameGrid from './components/GameGrid';
+import GameHeading from './components/GameHeading';
 import GenreList from './components/GenreList';
 import NavBar from './components/NavBar';
 import PlatformSelector from './components/PlatformSelector';
@@ -31,18 +32,21 @@ function App() {
 				</GridItem>
 			</Show>
 			<GridItem gridArea="main">
-				<Flex paddingLeft={2} marginBottom={5}>
-					<Box marginRight={5}>
-						<PlatformSelector
-							selectedPlatform={gameQuery.platform}
-							onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
-						/>
+				<Box paddingLeft={2}>
+					<GameHeading gameQuery={gameQuery} />
+					<Flex marginBottom={5}>
+						<Box marginRight={5}>
+							<PlatformSelector
+								selectedPlatform={gameQuery.platform}
+								onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })}
+							/>
+						</Box>
 						<SortSelector
 							sortOrder={gameQuery.sortOrder}
 							onSelectSortOrder={(sortOrder) => setGameQuery({ ...gameQuery, sortOrder })}
 						/>
-					</Box>
-				</Flex>
+					</Flex>
+				</Box>
 				<GameGrid gameQuery={gameQuery} />
 			</GridItem>
 		</Grid>
