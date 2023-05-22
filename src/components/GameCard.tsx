a057428d4e7c1b103383f00e41904e159b7d1e40
Tran-Duc-Hoa <tranduchoa611@gmail.com>
2023-05-22 13:11:38 +0700

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,9 +1,9 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 
-import { Game } from "../hooks/useGames";
-import getCroppedImageUrl from "../services/image-url";
-import CriticScore from "./CriticScore";
-import PlatformIconList from "./PlatformIconList";
+import { Game } from '../hooks/useGames';
+import getCroppedImageUrl from '../services/image-url';
+import CriticScore from './CriticScore';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
 	game: Game;
@@ -14,13 +14,11 @@ const GameCard = ({ game }: Props) => {
 		<Card>
 			<Image src={getCroppedImageUrl(game.background_image)} />
 			<CardBody>
-				<Heading fontSize="2xl">{game.name}</Heading>
-				<HStack justifyContent="space-between">
-					<PlatformIconList
-						platforms={game.parent_platforms.map((p) => p.platform)}
-					/>
+				<HStack justifyContent="space-between" marginBottom={3}>
+					<PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
 					<CriticScore score={game.metacritic} />
 				</HStack>
+				<Heading fontSize="2xl">{game.name}</Heading>
 			</CardBody>
 		</Card>
 	);
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -21,10 +21,10 @@ const GameGrid = ({ gameQuery }: Props) => {
 					sm: 1,
 					md: 2,
 					lg: 3,
-					xl: 5,
+					xl: 4,
 				}}
 				padding="10px"
-				spacing={3}
+				spacing={6}
 			>
 				{isLoading &&
 					skeletons.map((skeleton, index) => (
